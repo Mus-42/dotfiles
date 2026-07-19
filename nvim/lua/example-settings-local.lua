8a429703ba3720179e77aff242d3fa07655a320f
Mus <favmake@gmail.com>
2026-07-19 18:18:08 +0300

    local settings example

---
+++ nvim/lua/example-settings-local.lua
@@ -0,0 +1,17 @@
+local S = {
+    lsp_servers = {
+        "zls",
+        "clangd",
+        "lua_ls",
+        -- "rust_analyzer",
+        -- "marksman",
+        -- "cmake",
+        -- "tinymist",
+        "pylsp",
+    },
+    ts_parsers =  {
+        "c", "lua", "python", "vim", "vimdoc", "rust", "zig", "cpp", "glsl", "cmake"
+    },
+};
+
+return S
